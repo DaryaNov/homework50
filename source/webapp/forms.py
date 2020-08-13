a78af1fda719a8903408dcd7687ea089a088facc
DaryaNov <dasha_novoselova98@mail.ru>
2020-08-13 17:52:52 +0600

    add  commit forms

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,11 +1,37 @@
 from django import forms
-from .models import Status,Type
+from .models import Article, Status,Type
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_slug
+
+
 
 BROWSER_DATETIME_FORMAT = '%Y-%m-%dT%H:%M'
 
-class ArticleForm(forms.Form):
-    description = forms.CharField(max_length=200, required=True, label='Описание')
-    maxdescription = forms.CharField(max_length=3000, required=True, label='Подробное описание', widget=forms.Textarea)
+
+def at_least_5(string):
+    if len(string) < 5:
+        raise ValidationError('This value is too short!')
+
+
+
+def desc(string):
+    try:
+        validate_slug(string)
+    except ValidationError:
+        print("invalid input passed to validate_slug()")
+
+
+
+def word(words):
+    if words.find('secret') != -1:
+        raise ValidationError("You cannot enter the word secret!")
+
+
+
+
+class ArticleForm(forms.ModelForm):
+    description = forms.CharField(max_length=200, required=True, label='Описание',validators=(at_least_5,))
+    maxdescription = forms.CharField(max_length=3000, required=True, label='Подробное описание', widget=forms.Textarea,validators=(desc,word,))
     status = forms.ModelChoiceField(queryset=Status.objects.all(),  label='Статус')
     types = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False,label='Тип')
     publish_at = forms.DateTimeField(required=False, label='Время публикации',
@@ -13,3 +39,9 @@ class ArticleForm(forms.Form):
                                                     '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d %H:%M',
                                                     '%Y-%m-%d %H:%M:%S'],
                                      widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+
+
+
+    class Meta:
+        model = Article
+        fields = ['description', 'maxdescription', 'status', 'types', 'publish_at']
