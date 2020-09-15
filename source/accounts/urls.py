915e51874832fe64a8ebf72b3d2aaeb4e9bfda27
DaryaNov <dasha_novoselova98@mail.ru>
2020-09-15 15:35:58 +0600

    добавление страниц для редактирование профиля и смены пароля

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accounts.views import RegisterView,RegisterActivateView,UserDetailView
+from accounts.views import RegisterView,RegisterActivateView,UserDetailView,UserPasswordChangeView,UserChangeView
 app_name = 'accounts'
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='create'),
 
     path('activate/<uuid:token>/', RegisterActivateView.as_view(), name='activate'),
-    path('<int:pk>/', UserDetailView.as_view(), name='view')
+    path('<int:pk>/', UserDetailView.as_view(), name='view'),
+    path('<int:pk>/update/', UserChangeView.as_view(), name='update'),
+    path('<int:pk>/password-change/', UserPasswordChangeView.as_view(), name='password_update')
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,17 +1,18 @@
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
-from django.views.generic import CreateView, View
+from django.views.generic import CreateView, View, UpdateView
 from django.conf import settings
 from django.views.generic import DetailView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from accounts.forms import MyUserCreationForm
+from accounts.forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm, PasswordChangeForm
 from webapp.views.base_view import ListView
-from .models import  Profile,AuthToken
+from .models import Profile, AuthToken
 
 
 class RegisterActivateView(View):
@@ -49,8 +50,6 @@ class UserDetailView(LoginRequiredMixin, DetailView):
         return super().get_context_data(**kwargs)
 
 
-
-
 class RegisterView(CreateView):
     model = User
     template_name = 'user_create.html'
@@ -68,3 +67,53 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('index')
         return next_url
+
+
+class UserChangeView(UpdateView):
+    model = get_user_model()
+    form_class = UserChangeForm
+    template_name = 'user_update.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        if 'profile_form' not in kwargs:
+            kwargs['profile_form'] = self.get_profile_form()
+        return super().get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        profile_form = self.get_profile_form()
+        if form.is_valid() and profile_form.is_valid():
+            return self.form_valid(form, profile_form)
+        else:
+            return self.form_invalid(form, profile_form)
+
+    def form_valid(self, form, profile_form):
+        form.save()
+        profile_form.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, profile_form):
+        context = self.get_context_data(form=form, profile_form=profile_form)
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        return reverse('accounts:detail', kwargs={'pk': self.object.pk})
+
+    def get_profile_form(self):
+        form_kwargs = {'instance': self.object.profile}
+        if self.request.method == 'POST':
+            form_kwargs['data'] = self.request.POST
+            form_kwargs['files'] = self.request.FILES
+        return ProfileChangeForm(**form_kwargs)
+
+
+class UserPasswordChangeView(UpdateView):
+    model = get_user_model()
+    template_name = 'user_update_password.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
+
+    def get_success_url(self):
+        return reverse('accounts:login')
